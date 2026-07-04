5d124bf7910e18f991ad98482071a17d8aa036cd
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-07-04 16:35:47 -0600

    feat: create book arr and book object

---
+++ index.js
@@ -0,0 +1,33 @@
+// Books Arr
+const books = [];
+
+// Book Object
+function Book(title, author, pages, progress) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.progress = progress;
+  this.id = crypto.randomUUID();
+}
+
+Book.prototype.editBook = function (
+  newTitle,
+  newAuthor,
+  newPages,
+  newProgress,
+) {
+  this.title = newTitle;
+  this.author = newAuthor;
+  this.pages = newPages;
+  this.progress = newProgress;
+};
+
+// function to add book to library
+function addBookToLibrary(title, author, pages, progress) {
+  let book = new Book(title, author, pages, progress);
+  books.push(book);
+}
+
+addBookToLibrary("The merchant of venice", "Shakespear", 324, 20);
+addBookToLibrary("No country for old men", "Cormac Mcarthy", 200, 100);
+addBookToLibrary("Blood Meridian", "Cormac Mcarthy", 423, 150);
