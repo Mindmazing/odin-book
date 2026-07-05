a96aa93f1709d5da5855bc4866aabdccf46bdfbd
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-07-05 14:44:23 -0600

    feat: add book object to document with addToSite() func

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BOOKS_CONTAINER = document.querySelector(".book-container");
+const BOOKS_CONTAINER = document.querySelector(".books-container");
 
 // Books Arr
 const books = [];
@@ -13,6 +13,7 @@ function Book(title, author, pages, progress, description) {
   this.id = crypto.randomUUID();
 }
 
+// this function lets edition in a Book oject
 Book.prototype.editBook = function (
   newTitle = this.title,
   newAuthor = this.author,
@@ -26,10 +27,14 @@ Book.prototype.editBook = function (
 };
 
 Book.prototype.addToSite = function () {
+  // create bookcard element
   let bookCard = document.createElement("div");
   bookCard.classList.add("book-card");
 
-  let randomHeaderImg = Math.floor(Math.random * 4) - 1;
+  // assign random int for header image selection
+  let randomHeaderImg = Math.floor(Math.random() * 4) + 1;
+
+  // create innerHTML for bookcard element
   bookCard.innerHTML = `
     <div class="book-header">
             <img src="./images/book-header-${randomHeaderImg}.jpg" alt="books"/>
@@ -57,14 +62,40 @@ Book.prototype.addToSite = function () {
               <button class="edit-btn">Edit</button>
             </div>
   `;
+
+  // append bookcard to BOOKS_CONTAINER
+  BOOKS_CONTAINER.appendChild(bookCard);
 };
 
 // function to add book to library
-function addBookToLibrary(title, author, pages, progress) {
-  let book = new Book(title, author, pages, progress);
+function addBookToLibrary(title, author, pages, progress, description) {
+  let book = new Book(title, author, pages, progress, description);
   books.push(book);
 }
 
-addBookToLibrary("The merchant of venice", "Shakespear", 324, 20);
-addBookToLibrary("No country for old men", "Cormac Mcarthy", 200, 100);
-addBookToLibrary("Blood Meridian", "Cormac Mcarthy", 423, 150);
+// add some placeholder books to the books arr
+addBookToLibrary(
+  "The merchant of venice",
+  "Shakespear",
+  324,
+  20,
+  "The ultimate book created by shakespear",
+);
+addBookToLibrary(
+  "No country for old men",
+  "Cormac Mcarthy",
+  200,
+  100,
+  "A book about a killer finding a cowboy who stole cartel money.",
+);
+addBookToLibrary(
+  "Blood Meridian",
+  "Cormac Mcarthy",
+  423,
+  150,
+  "Dont ever read this book if you are weak",
+);
+
+for (let book of books) {
+  book.addToSite();
+}
