59a76fc054a6d8926142d0367d2a6006329b321c
mindmazing <67432331+Mindmazing@users.noreply.github.com>
2026-07-05 14:15:06 -0600

    feat: add function to add book to books-container
    function creates the book-card element, assigns a
    random img from the images/ directory.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const BOOKS_CONTAINER = document.querySelector(".book-container");
+
 // Books Arr
 const books = [];
 
@@ -12,15 +14,49 @@ function Book(title, author, pages, progress, description) {
 }
 
 Book.prototype.editBook = function (
-  newTitle,
-  newAuthor,
-  newPages,
-  newProgress,
+  newTitle = this.title,
+  newAuthor = this.author,
+  newPages = this.pages,
+  newProgress = this.progress,
 ) {
   this.title = newTitle;
   this.author = newAuthor;
   this.pages = newPages;
   this.progress = newProgress;
+};
+
+Book.prototype.addToSite = function () {
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  let randomHeaderImg = Math.floor(Math.random * 4) - 1;
+  bookCard.innerHTML = `
+    <div class="book-header">
+            <img src="./images/book-header-${randomHeaderImg}.jpg" alt="books"/>
+          </div>
+          <div class="book-details">
+            <div book-title>
+              <h2>${this.title}</h2>
+              <span>${this.author}</span>
+            </div>
+            <div class="book-description">
+              <p>
+                ${this.description}
+              </p>
+            </div>
+            <div class="book-progress">
+              <progress max="${this.pages}" value="${this.progress}" id="book-progress"></progress>
+              <div class="progress-numbers">
+                <span>0</span>
+                <span>${this.progress}</span>
+                <span>${this.pages}</span>
+              </div>
+            </div>
+            <div class="book-buttons">
+              <button class="delete-btn">Delete</button>
+              <button class="edit-btn">Edit</button>
+            </div>
+  `;
 };
 
 // function to add book to library
